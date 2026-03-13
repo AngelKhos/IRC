9646407cc1eefe60da895a9459797d18cc2e0c9d
The-Lizzard-Wizard <gaelchauvet@orange.fr>
2026-03-13 13:32:58 +0100

    UPDATE server constructor

--- src/class/cpp/server.cpp
+++ src/class/cpp/server.cpp
@@ -1,10 +1,18 @@
 #include "server.hpp"
 #include <iostream>
-
-Server::Server()
-{
-}
+Server::Server(unsigned short Pport, std::string Ppass) :
+	port(Pport),
+	password(Ppass)
+{}
 
 Server::~Server()
 {
 }
+
+void Server::setPort(unsigned short Pport) { port = Pport; }
+
+unsigned short Server::getPort() { return (port); }
+
+void Server::setPassword(std::string Ppass) { password = Ppass; }
+
+std::string Server::getPassword() { return (password); }
--- src/class/hpp/server.hpp
+++ src/class/hpp/server.hpp
@@ -1,13 +1,21 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <string>
+
 class Server
 {
 	public:
-		Server();
+		Server(unsigned short port, std::string Ppass);
 		~Server();
 
+		void setPort(unsigned short Pport);
+		unsigned short getPort();
+		void setPassword(std::string Ppass);
+		std::string getPassword();
 	private:
+		unsigned short port;
+		std::string password;
 };
 
 #endif //SERVER_HPP
