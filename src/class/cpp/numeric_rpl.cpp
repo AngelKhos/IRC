75f713fdf86899a4628446ac7c17e6f44cd4f672
The-Lizzard-Wizard <gaelchauvet@orange.fr>
2026-03-30 16:21:51 +0200

    ADD rpl_topic

--- src/class/cpp/numeric_rpl.cpp
+++ src/class/cpp/numeric_rpl.cpp
@@ -2,6 +2,9 @@
 #include <sys/socket.h>
 #include <server.hpp>
 
+//===============================================================
+//reply
+
 void NumRpl::rpl001(int fd, str servName, str nick)
 {
 	str rpl = str("001 ") + nick +
@@ -11,23 +14,32 @@ void NumRpl::rpl001(int fd, str servName, str nick)
 	send(fd, rpl.c_str(), rpl.size(), 0);
 }
 
+void NumRpl::rpl353(int fd,Channel ch, str nick)
+{
+	str rpl = str("353 ") + nick +
+	ch.getName() + str(" :");
+	for (std::set<Client *>::iterator it = ch.getUsers().begin(); it != ch.getUsers().end(); it++)
+	{
+		if (ch.getOpUsers().count(*it) > 0)
+		rpl += "@";
+		rpl += (*it)->nickName;
+	}
+	send(fd, rpl.c_str(), rpl.size(), 0);
+}
+
+void NumRpl::rpl332(int fd, Channel ch, str nick)
+{
+	str rpl = str("332 ") + nick + 
+	ch.getName() + str(" :") + ch.getTopic();
+	send(fd, rpl.c_str(), rpl.size(), 0);
+}
+
+//===============================================================
+//error
+
 void NumRpl::err421(int fd, str com, str nick)
 {
 	str rpl = str("421 ") + nick + 
 		com + str(" :Unknown command");
 	send(fd, rpl.c_str(), rpl.size(), 0);
 }
-
-void NumRpl::rpl353(int fd,Channel ch, str nick)
-{
-	str rpl = str("353 ") + nick +
-		ch.getName() + str(" :");
-	for (std::set<Client *>::iterator it = ch.getUsers().begin(); it != ch.getUsers().end(); it++)
-	{
-		if (ch.getOpUsers().count(*it) > 0)
-			rpl += "@";
-		rpl += (*it)->nickName;
-	}
-	send(fd, rpl.c_str(), rpl.size(), 0);
-}
-
