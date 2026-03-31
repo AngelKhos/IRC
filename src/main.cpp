7edc85d3ed4632bd2040e0c5008609981f790e54
The-Lizzard-Wizard <gaelchauvet@orange.fr>
2026-03-31 14:55:19 +0200

    split test

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,46 @@
 
 void sigHandler(int) {}
 
+std::vector<std::string> cmd_split(std::string args)
+{
+	std::vector<std::string> vec;
+	for (size_t i = 0; i < args.size(); i++)
+	{
+
+		if (args[i] != ':')
+		{
+			if (args[i] != ' ' || i != args.size() - 1)
+				continue ;
+			else
+			{
+				// if (args[i] != '\0')
+				i++;
+				vec.push_back(args.substr(0, i));
+				args.erase(0, i);
+				i = 0;
+			}
+		}
+		else
+		{
+			vec.push_back(args);
+			return (vec);
+		}
+	}
+	return (vec);
+}
+
 int main(int argc, char **argv)
 {
 	signal(SIGINT, sigHandler);
 	if (argc != 3)
 		return (1);
 	Server ircServer((unsigned short)atoi(argv[1]), argv[2]);
+
+	std::vector<std::string> vec_test = cmd_split("bonjour ruojnob hello world");
+	for (std::vector<std::string>::iterator it = vec_test.begin(); it <= vec_test.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
 
 	try
 	{
