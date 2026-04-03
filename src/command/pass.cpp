5148f674611dc4b62c543fd3b3dcc13eb77e1287
authomas <authomas@student.42lyon.fr>
2026-04-03 15:05:23 +0200

    command pass

--- src/command/pass.cpp
+++ src/command/pass.cpp
@@ -7,12 +7,12 @@ void Server::pass(std::vector<std::string> args, int client_fd)
         updateClient(client_fd, Rep.err462(clients[client_fd]->nickName));
         return ;
     }
-    if (args == "\r\n")
+    if (args[0] == "\r\n")
     {
         updateClient(client_fd, Rep.err461("PASS", clients[client_fd]->nickName));
         return ;
     }
-    if (args == password)
+    if (args[0] == password)
         clients[client_fd]->has_pass = true;
 
     //si le password est invalide, c'est ignoré
