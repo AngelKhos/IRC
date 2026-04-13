b6c1b2be199f056c3230e4131523033f525f602b
The-Lizzard-Wizard <gaelchauvet@orange.fr>
2026-04-13 17:20:04 +0200

    FIX segfault in serveur destructor

--- src/class/cpp/server.cpp
+++ src/class/cpp/server.cpp
@@ -98,9 +98,13 @@ void Server::stopServer(int x)
 	// 	Client &c = clients[i];
 	// 	close(c.getClientFd());
 	// }
-	for (std::map<int, Client *>::iterator it = clients.begin(); it != clients.end(); it++) //test broadcast
+	for (std::map<int, Client *>::iterator it = clients.begin(); it != clients.end(); it++)
 	{
 		disconnectClient(it->second->client_fd);
+	}
+	for (std::set<Channel *>::iterator it = channels.begin(); it != channels.end(); it++)
+	{
+		delete *it;
 	}
 	//faudra boucler sur le set des channels pour les del aussi
 	if (epoll.getEpollFd() != -1)
@@ -126,7 +130,7 @@ void Server::disconnectClient(int fd)
 	epoll.ctl_del(fd);
 	clients[fd]->Disconnect();
 	delete clients[fd];
-	clients.erase(fd);
+	//clients.erase(fd); // cette ligne fesais segfault quand on quittai avec un user dans un channel
 }
 
 void Server::updateClient(int fd, std::string message) //fait en sorte que si on a qqch a envoyer, ca set EPOLLOUT
